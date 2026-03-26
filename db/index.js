bc518866481f03b3fbbf33c26250c54d74cda10c
MyMac <mymac@MyMacs-MacBook-Pro-2.local>
2026-03-25 20:45:02 -0700

    Disable SSL for internal Railway Postgres

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('localhost') ? false : { rejectUnauthorized: false },
+  ssl: false,
 });
 
 async function initSchema() {
